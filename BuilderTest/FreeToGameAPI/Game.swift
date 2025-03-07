da3249c2af3a3860268fe9fd477df797cfa76700
Dan Giralte <daniel.giralte@porsche.digital>
2025-03-07 12:58:46 -0600

    Add Game struct

---
+++ BuilderTest/FreeToGameAPI/Game.swift
@@ -0,0 +1,20 @@
+//
+//  Game.swift
+//  BuilderTest
+//
+//  Created by Dan Giralte on 3/7/25.
+//
+
+struct Game {
+    let id : Int
+    let title : String
+    let thumbnail : String
+    let short_description : String
+    let game_url : String
+    let genre : String
+    let platform : String
+    let publisher : String
+    let developer: String
+    let release_date : String
+    let freetogame_profile_url : String
+}
